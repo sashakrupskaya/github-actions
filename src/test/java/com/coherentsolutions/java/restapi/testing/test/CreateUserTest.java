1d67ee6a542e9dc2d88e32a56277442c6449da6c
Alexandra <alexandrakrupskaya@coherentsolutions.com>
2025-04-30 20:03:05 +0300

    rest

--- src/test/java/com/coherentsolutions/java/restapi/testing/test/CreateUserTest.java
+++ src/test/java/com/coherentsolutions/java/restapi/testing/test/CreateUserTest.java
@@ -26,7 +26,7 @@ public class CreateUserTest {
     protected static final Logger logger = LoggerFactory.getLogger(CreateUserTest.class);
     @BeforeEach
     public void setUp() throws IOException, InterruptedException {
-        client = ClientFactory.createClient(ApacheClient.class);
+        client = ClientFactory.createClient(RestAssuredClient.class);
         client.restartDockerContainer();
         client.resetHttpClient();
         client.refreshTokensAfterReset();
