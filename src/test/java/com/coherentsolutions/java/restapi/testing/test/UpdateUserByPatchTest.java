3115363918d86739b1ca614bd6e8fc9871eb3eb7
Alexandra <alexandrakrupskaya@coherentsolutions.com>
2025-05-02 12:21:32 +0300

    RestAssured

--- src/test/java/com/coherentsolutions/java/restapi/testing/test/UpdateUserByPatchTest.java
+++ src/test/java/com/coherentsolutions/java/restapi/testing/test/UpdateUserByPatchTest.java
@@ -33,7 +33,7 @@ public class UpdateUserByPatchTest {
 
     @BeforeEach
     public void setUp() throws IOException, InterruptedException {
-        client = ClientFactory.createClient(ApacheClient.class);
+        client = ClientFactory.createClient(RestAssuredClient.class);
         client.restartDockerContainer();
         client.resetHttpClient();
         client.refreshTokensAfterReset();
